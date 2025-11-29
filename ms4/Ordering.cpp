a2749e537614118ff8c5c740256e8ef10f7d5a71
uchennachibueze <emmachi72.ec@gmail.com>
2025-11-29 11:13:42 -0500

    Implemented the ordering.cpp

--- ms4/Ordering.cpp
+++ ms4/Ordering.cpp
@@ -1,3 +1,6 @@
+#include <iomanip>
+#include <iostream>
+#include <fstream>
 #include "Ordering.h"
 using namespace std;
 
@@ -8,30 +11,128 @@ namespace seneca {
 	}
 	void Ordering::printTotal(std::ostream& ostr, double total) const
 	{
+		ostr << "Total: " << fixed << setprecision(2) << total << endl;
+		ostr << "Tax: " << fixed << setprecision(2) << total * Tax << endl;
+		ostr << "Total+Tax: " << fixed << setprecision(2) << total + Tax << endl;
+		ostr << "===============================" << endl;
 	}
 	size_t Ordering::countRecords(const char* file) const
 	{
-		return size_t();
+		size_t newlineCounter = 0;
+		std::ifstream resFiles(file);
+		char ch{};
+		while (resFiles.get(ch)) {
+			if (ch == '\n') {
+				newlineCounter++;
+			}
+		}
+		return newlineCounter;
 	}
-	Ordering::Ordering(const char* drinkFileName, const char* FoodFileName)
+	Ordering::Ordering(const char* drinkFileName, const char* FoodFileName) 
+	:	m_bill_sn{ 1 }, 
+		m_billableCounter{ 0 },
+		m_drink_items{ nullptr },
+		m_food_items{ nullptr },
+		m_drinkCounter{ 0 },
+		m_foodCounter{ 0 }
 	{
+
+		size_t numOfDrinkRec = countRecords(drinkFileName);
+		size_t numOfFoodRec = countRecords(FoodFileName);
+		//cout << "Number of drink records: " << numOfDrinkRec << endl;
+		//cout << "Number of food records: " << numOfFoodRec << endl;
+		ifstream drinkFile(drinkFileName);
+		ifstream foodFile(FoodFileName);
+
+		if (!drinkFile || !foodFile) {
+			m_drink_items = nullptr;
+			m_food_items = nullptr;
+			m_drinkCounter = 0;
+			m_foodCounter = 0;
+			return;
+		}
+			
+		Drink* drinks = new Drink[numOfDrinkRec];
+		Food* foods = new Food[numOfFoodRec];
+
+		size_t drinkReadResult = 0;
+		size_t foodReadResult = 0;
+
+		for (size_t i = 0; i < numOfDrinkRec; i++) {
+			drinks[i].read(drinkFile);
+			if (!drinkFile.fail()) {
+				drinkReadResult++;
+			}
+			else {
+				break;
+			}
+		}
+			
+		for (size_t j = 0; j < numOfFoodRec; j++) {
+			foods[j].read(foodFile);
+			if (!foodFile.fail()) {
+				foodReadResult++;
+			} else {
+				break;
+			}
+		}
+		
+		if (drinkReadResult != numOfDrinkRec || foodReadResult != numOfFoodRec) {
+			delete[] drinks;
+			delete[] foods;
+			m_drink_items = nullptr;
+			m_food_items = nullptr;
+			m_drinkCounter = 0;
+			m_foodCounter = 0;
+			return;
+		}
+
+		m_drink_items = drinks;
+		m_food_items = foods;
+		m_drinkCounter = numOfDrinkRec;
+		m_foodCounter = numOfFoodRec;
 	}
-	Ordering::~Ordering()
-	{
+	Ordering::~Ordering() {
+		delete[] m_drink_items;
+		delete[] m_food_items;
+
+		for (size_t i = 0; i < m_billableCounter; i++) {
+			delete m_bill_items[i];
+		}
 	}
+
 	size_t Ordering::noOfBillItems() const
 	{
-		return size_t();
+		return m_billableCounter;
+	}
+
+	Ordering::operator bool() const
+	{
+		return m_food_items != nullptr && m_drink_items != nullptr;
 	}
 	bool Ordering::hasUnsavedBill() const
 	{
-		return false;
+		return m_billableCounter > 0;
 	}
 	void Ordering::listFoods() const
 	{
+		cout << "List of Available Meals " << endl;
+		cout << "========================" << endl;
+		for (size_t i = 0; i < m_foodCounter; i++) {
+			m_food_items[i].print();
+			cout << endl;
+		}
+		cout << "========================" << endl;
 	}
 	void Ordering::listDrinks() const
 	{
+		cout << "List of Available Drinks " << endl;
+		cout << "========================" << endl;
+		for (size_t i = 0; i < m_drinkCounter; i++) {
+			m_food_items[i].print();
+			cout << endl;
+		}
+		cout << "========================" << endl;
 	}
 	void Ordering::orderFood()
 	{
